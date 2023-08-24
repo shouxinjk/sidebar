0b18221268c6be547d2130893d4811494d3ba4e1
Alex Chew <alex_chew@msn.com>
2023-08-24 18:27:27 +0800

    enable wework workplace

--- src/views/system/loginmini/OAuth2Login.vue
+++ src/views/system/loginmini/OAuth2Login.vue
@@ -70,9 +70,10 @@
         let token = route.query.oauth2LoginToken;
         //执行登录操作
         thirdLogin({ token, thirdType: route.query.thirdType });
-      } else if (env.value.wework) { // 企微服务商服务商模式：带有code及state，直接进入 企业微信服务商后端处理
+      } else if (env.value.wework && route.query.code ) { // 企微服务商服务商模式：带有code及state，获取用户信息并完成登录。适用于侧边栏菜单，配置URL为构建好的oauth链接
         let code = route.query.code;
         let state = route.query.state;
+        console.log("route.query.",route.query);
         console.log("target location href.",SUITE_ID, code, state, window.location.href);
         //执行登录操作
         thirdLogin({ //传递suiteId、code、state完成企业微信登录
@@ -80,6 +81,8 @@
           code: code,
           state: state,
           thirdType: "wework" }); 
+      }else if (env.value.wework) { //企业微信服务商模式：从PC企业微信或手机端企业微信 工作台进入 SaaS，需要先构建oauth授权链接再完成登录
+        sysOAuth2Login('wework');
       }else if (env.value.wxWork) {
         sysOAuth2Login('wechat_enterprise');
       } else if (env.value.dingtalk) {
