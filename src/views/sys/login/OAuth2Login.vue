5bf7d8ad0abcadb85c463818a4a81a890763fe46
Alex Chew <alex_chew@msn.com>
2023-08-24 23:26:54 +0800

    bug fix

--- src/views/sys/login/OAuth2Login.vue
+++ src/views/sys/login/OAuth2Login.vue
@@ -72,7 +72,11 @@
     console.log("try doOAuth2Login", env, route.query);
     if (env.value.thirdApp) {
       // 判断是否携带了Token，是就说明登录成功
-      if (env.value.wework && route.query.code ) { // 企微服务商服务商模式：带有code及state，获取用户信息并完成登录。适用于侧边栏菜单，配置URL为构建好的oauth链接
+      if (route.query.oauth2LoginToken) {
+        let token = route.query.oauth2LoginToken;
+        //执行登录操作
+        thirdLogin({ token, thirdType: route.query.thirdType });
+      }else if (env.value.wework && route.query.code ) { // 企微服务商服务商模式：带有code及state，获取用户信息并完成登录。适用于侧边栏菜单，配置URL为构建好的oauth链接
         let code = route.query.code;
         let state = route.query.state;
         console.log("route.query.",route.query);
@@ -83,12 +87,8 @@
           code: code,
           state: state,
           thirdType: "wework" }); 
-      }else if (env.value.wework) { //企业微信服务商模式：从PC企业微信或手机端企业微信 工作台进入 SaaS，需要先构建oauth授权链接再完成登录
+      } else if (env.value.wework) { //企业微信服务商模式：从PC企业微信或手机端企业微信 工作台进入 SaaS，需要先构建oauth授权链接再完成登录
         sysOAuth2Login('wework');
-      }else if (route.query.oauth2LoginToken) {
-        let token = route.query.oauth2LoginToken;
-        //执行登录操作
-        thirdLogin({ token, thirdType: route.query.thirdType });
       } else if (env.value.wxWork) {
         sysOAuth2Login('wechat_enterprise');
       } else if (env.value.dingtalk) {
--- src/views/system/loginmini/OAuth2Login.vue
+++ src/views/system/loginmini/OAuth2Login.vue
@@ -68,7 +68,11 @@
    */
   function doOAuth2Login() {
     if (env.value.thirdApp) {
-      if (env.value.wework && route.query.code ) { // 企微服务商服务商模式：带有code及state，获取用户信息并完成登录。适用于侧边栏菜单，配置URL为构建好的oauth链接
+      if (route.query.oauth2LoginToken) { // 判断是否携带了Token，是就说明登录成功
+        let token = route.query.oauth2LoginToken;
+        //执行登录操作
+        thirdLogin({ token, thirdType: route.query.thirdType });
+      } else if (env.value.wework && route.query.code ) { // 企微服务商服务商模式：带有code及state，获取用户信息并完成登录。适用于侧边栏菜单，配置URL为构建好的oauth链接
         let code = route.query.code;
         let state = route.query.state;
         console.log("route.query.",route.query);
@@ -79,12 +83,8 @@
           code: code,
           state: state,
           thirdType: "wework" }); 
-      }else if (env.value.wework) { //企业微信服务商模式：从PC企业微信或手机端企业微信 工作台进入 SaaS，需要先构建oauth授权链接再完成登录
+      } else if (env.value.wework) { //企业微信服务商模式：从PC企业微信或手机端企业微信 工作台进入 SaaS，需要先构建oauth授权链接再完成登录
         sysOAuth2Login('wework');
-      }else if (route.query.oauth2LoginToken) { // 判断是否携带了Token，是就说明登录成功
-        let token = route.query.oauth2LoginToken;
-        //执行登录操作
-        thirdLogin({ token, thirdType: route.query.thirdType });
       } else if (env.value.wxWork) {
         sysOAuth2Login('wechat_enterprise');
       } else if (env.value.dingtalk) {
